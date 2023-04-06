089c4541c773ed927db113ecaff9204374cc3b5c
Taisei <taisei96.web@gmail.com>
2023-04-06 14:55:40 +0900

    JS修正

--- js/main_backup.js
+++ js/main_backup.js
@@ -4,22 +4,15 @@ const taskInput  = document.querySelector('.task_input');
 const taskSubmit = document.querySelector('.task_submit');
 const taskList   = document.querySelector('.task-list');
 
-
+// タスク追加ボタンをクリックした時の処理
 taskSubmit.addEventListener('click', e => {
+
+  // submitイベントの動作を止める
   e.preventDefault();
 
   // 未入力ならタスクを追加しない
   if (taskInput.value === '') return
-  
-  taskCreate();
 
-  // タスク追加後にフォームを空にする
-  taskInput.value = ''
-})
-
-function taskCreate() {
-
-  // タスクを格納するリスト
   const listItem  = document.createElement('li');
   taskList.appendChild(listItem);
 
@@ -33,6 +26,9 @@ function taskCreate() {
   const showTask  = listItem.appendChild(taskValue);
 
   showTask.textContent = taskInput.value;
+
+  // タスク追加後にフォームを空にする
+  taskInput.value = ''
 
   // 削除ボタン作成
   const deleteBtn     = document.createElement('button');
@@ -52,8 +48,17 @@ function taskCreate() {
   // タスク削除ボタンをクリックした時の処理
   deleteBtn.addEventListener('click', e => {
     e.preventDefault();
+
     // タスクを削除
     listItem.remove();
+
   })
 
-}
+})
+
+
+
+
+
+
+
